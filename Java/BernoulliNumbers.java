26d57869d1c372ac68a4f967eebe687244267d2c
Martin Titz <Martin Titz>
2023-10-03 12:21:30 +0200

    Add comments and command line argument to BernoulliNumbers class

--- Java/BernoulliNumbers.java
+++ Java/BernoulliNumbers.java
@@ -1,3 +1,8 @@
+/* Calculates a list of Bernoulli numbers as fractions using BigFraction class.
+   See Shanjie Zhang, Jianming Jin, "Computation of Special Functions",
+   formulas (1.1.4) and (1.1.5) and FORTRAN-77 subroutine BERNOA.
+*/
+
 public class BernoulliNumbers
 {
     public static BigFraction[] calculateBernoulliNumbers(int n)
@@ -32,7 +37,15 @@ public class BernoulliNumbers
 
     public static void main(String[] args)
     {
-        BigFraction[] bern = calculateBernoulliNumbers(30);
+        int limit = 30;
+        if (args.length > 0) {
+            limit = Integer.parseInt(args[0]);
+            if (args.length >= 2) {
+                System.err.println("Command line error");
+                return;
+            }
+        }
+        BigFraction[] bern = calculateBernoulliNumbers(limit);
         printTable(bern);
     }
 }

61bfb43d2307a3abf9a9a8d543f869709083eeb3
Martin Titz <Martin Titz>
2023-12-02 14:58:21 +0100

    Change variables in Micropuzzle07a from long to int

--- Java/Micropuzzle07a.java
+++ Java/Micropuzzle07a.java
@@ -1,31 +1,56 @@
-/* Program Micropuzzle07.java to solve micropuzzle 7 from
+/* Program Micropuzzle07a.java to solve micropuzzle 7 from
    J. J. Clessa "Math and Logic Puzzles for PC Enthusiasts"
+   More perfect squares
 */
 
 import java.util.Set;
 import java.util.HashSet;
 
-public class Micropuzzle07
+public class Micropuzzle07a
 {
+    public static final int gcd(int x, int y)
+    {
+        while (y != 0) {
+            int tmp = y;
+            y = x % y;
+            x = tmp;
+        }
+        return Math.abs(x);
+    }
+
+    public static final int integerSqrt(int n)
+    {
+        if (n <= 0)
+            return 0;
+        int x = n;
+        while (true) {
+            int y = (x + n / x) >> 1;
+            if (y < x)
+                x = y;
+            else
+                return x;
+        }
+    }
+
     public static void main(String[] args)
     {
-        long nMax = 2500;
-        Set<Long> squares = new HashSet<>();
-        for (long n = 1; n <= nMax; n += 2) {
+        final int nMax = 2500;
+        Set<Integer> squares = new HashSet<>();
+        for (int n = 1; n <= nMax; n += 2) {
             squares.add(n * n);
         }
 
-        for (long a = 1; a < nMax; a += 2) {
-            long a_2 = a * a;
-            long deltaLimit = (nMax * nMax - a_2) / 2;
-            long delta = 0;
+        for (int a = 1; a < nMax; a += 2) {
+            final int aSquare = a * a;
+            final int deltaLimit = (nMax * nMax - aSquare) / 2;
+            int delta = 0;
             while ((delta += 4) <= deltaLimit) {
-                if (NumberTheory.gcd(a, delta) > 1) continue;  // allow only 'primitive' solutions
-                if (squares.contains(a_2 + delta) && squares.contains(a_2 + 2*delta)) {
-                    long b = NumberTheory.integerSqrt(a_2 +   delta);
-                    long c = NumberTheory.integerSqrt(a_2 + 2*delta);
+                if (gcd(a, delta) > 1) continue;  // allow only 'primitive' solutions
+                if (squares.contains(aSquare + delta) && squares.contains(aSquare + 2*delta)) {
+                    int b = integerSqrt(aSquare +   delta);
+                    int c = integerSqrt(aSquare + 2*delta);
                     System.out.printf("%9d =%5d^2, %9d =%5d^2, %9d =%5d^2  with difference %9d%n",
-                            a_2, a, b * b, b, c * c, c, delta);
+                            aSquare, a, b * b, b, c * c, c, delta);
                 }
             }
         }

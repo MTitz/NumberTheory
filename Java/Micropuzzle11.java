cbcc6e35d7d4007961517f96b4c9c42b89d4c902
Martin Titz <Martin Titz>
2023-12-18 15:12:40 +0100

    Use test for perfect squares from NumberTheory class in micropuzzle 11 solution

--- Java/Micropuzzle11.java
+++ Java/Micropuzzle11.java
@@ -1,9 +1,7 @@
 /* Program Micropuzzle11.java to solve micropuzzle 11 from
    J. J. Clessa "Math and Logic Puzzles for PC Enthusiasts"
+   The not-so-perfect square
 */
-
-import java.util.Set;
-import java.util.TreeSet;
 
 import static java.lang.Math.*;
 
@@ -11,25 +9,21 @@ public class Micropuzzle11
 {
     public static void main(String[] args)
     {
-        long nMax = 1000;
-        Set<Long> squares = new TreeSet<>();
-        for (long n = 1; n <= nMax; ++n) {
-            squares.add(n * n);
-        }
-        long dMax = round(floor(nMax / sqrt(10.0)));
-
-        for (long a = 1; a < nMax; ++a) {
+        long aMax = 1000;
+        for (long a = 1; a < aMax; ++a) {
             long a_2 = a * a;
+            long dMax = round(floor(a / sqrt(5.0)));
             for (long d = 1; d < dMax; ++d) {
                 long d_2 = d * d;
-                if (squares.contains(a_2 + 5 * d_2) && squares.contains(a_2 + 10 * d_2)) {
-                    if (NumberTheory.gcd(a, d) == 1) {
-                        long b = NumberTheory.integerSqrt(a_2 +  5 * d_2);
-                        long c = NumberTheory.integerSqrt(a_2 + 10 * d_2);
-                        System.out.println("Solution: (" + a + "/" + d + ")^2" +
-                                                   ", (" + b + "/" + d + ")^2" +
-                                                   ", (" + c + "/" + d + ")^2");
-                    }
+                if (NumberTheory.isSquare(a_2 - 5 * d_2) &&
+                    NumberTheory.isSquare(a_2 + 5 * d_2) &&
+                    NumberTheory.gcd(a, d) == 1) {
+                    long b = NumberTheory.integerSqrt(a_2 - 5 * d_2);
+                    long c = NumberTheory.integerSqrt(a_2 + 5 * d_2);
+                    System.out.println("Solution: (" + b + "/" + d + ")^2" +
+                                               ", (" + a + "/" + d + ")^2" +
+                                               ", (" + c + "/" + d + ")^2");
+                   
                 }
             }
         }

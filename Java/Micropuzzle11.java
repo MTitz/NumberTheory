ab988db5e0fdfcfa8a3bf2b901d3bbec6edd5d51
Martin Titz <Martin Titz>
2023-12-19 07:40:28 +0100

    Let main loop run until <= aMax instead of < aMax; Rename some variables

--- Java/Micropuzzle11.java
+++ Java/Micropuzzle11.java
@@ -10,20 +10,19 @@ public class Micropuzzle11
     public static void main(String[] args)
     {
         long aMax = 1000;
-        for (long a = 1; a < aMax; ++a) {
-            long a_2 = a * a;
+        for (long a = 1; a <= aMax; ++a) {
+            long aSquare = a * a;
             long dMax = round(floor(a / sqrt(5.0)));
-            for (long d = 1; d < dMax; ++d) {
-                long d_2 = d * d;
-                if (NumberTheory.isSquare(a_2 - 5 * d_2) &&
-                    NumberTheory.isSquare(a_2 + 5 * d_2) &&
+            for (long d = 1; d <= dMax; ++d) {
+                long dSquare = d * d;
+                if (NumberTheory.isSquare(aSquare - 5 * dSquare) &&
+                    NumberTheory.isSquare(aSquare + 5 * dSquare) &&
                     NumberTheory.gcd(a, d) == 1) {
-                    long b = NumberTheory.integerSqrt(a_2 - 5 * d_2);
-                    long c = NumberTheory.integerSqrt(a_2 + 5 * d_2);
+                    long b = NumberTheory.integerSqrt(aSquare - 5 * dSquare);
+                    long c = NumberTheory.integerSqrt(aSquare + 5 * dSquare);
                     System.out.println("Solution: (" + b + "/" + d + ")^2" +
                                                ", (" + a + "/" + d + ")^2" +
                                                ", (" + c + "/" + d + ")^2");
-                   
                 }
             }
         }

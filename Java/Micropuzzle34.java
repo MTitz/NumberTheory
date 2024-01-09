316760a1f8bb63c62a7827e7815ea60e87ef46f4
Martin Titz <Martin Titz>
2024-01-09 20:09:04 +0100

    Speed up solution for micropuzzle 34 and change from int to long

--- Java/Micropuzzle34.java
+++ Java/Micropuzzle34.java
@@ -5,11 +5,10 @@
 
 public class Micropuzzle34
 {
-    public static int power(int base, int exponent)
+    public static long power(long base, int exponent)
     {
-        int result = 1;
-        while (exponent != 0)
-        {
+        long result = 1;
+        while (exponent > 0) {
             if ((exponent & 1) != 0)
                 result *= base;
             exponent >>= 1;
@@ -18,12 +17,12 @@ public class Micropuzzle34
         return result;
     }
 
-    public static int number(final int[] digits)
+    public static long number(final int[] digits)
     {
         final int base = 10;
         if (digits == null)
             return 0;
-        int n = 0;
+        long n = 0;
         for (int i = digits.length-1; i >= 0; --i) {
             n *= base;
             n += digits[i];
@@ -33,24 +32,34 @@ public class Micropuzzle34
 
     public static void search(final int nDigits)
     {
-        final int nLow = power(10, nDigits-1);
-        final int nHigh = 10 * nLow;
+        if (nDigits <= 1)
+             return;
+        final long nHigh = power(10, nDigits-2);
         int[] digits = new int[nDigits];
         for (int a = 2; a <= 9; ++a) {
-            if (a == 5)
-                continue;
-            int dLimit = nHigh / a;
-            for (int d = nLow; d <= dLimit; ++d) {
-                if (d % 10 == 0)
+            for (int d0 = 1; d0 <= 9; ++d0) {
+                int dn = (d0 * a) % 10;
+                if (dn * a >= 10 || dn == 0 || dn * a >= d0 + 1 || (dn + 1) * a <= d0) {
+                    //System.err.println("No solution for a = " + a + ", d0 = " + d0 + ", dn = " + dn);
                     continue;
-                int n = d;
-                for (int i = 0; i < nDigits; ++i) {
-                    digits[nDigits-1-i] = n % 10;
-                    n /= 10;
                 }
-                n = number(digits);
-                if (d * a == n) {
-                    System.out.println("Solution: " + d + " * " + a + " = " + n);
+                //System.err.println("Searching a = " + a + ", d0 = " + d0 + ", dn = " + dn);
+                final long dLimit = 10 * nHigh * (d0 + 1) / a;
+                for (long dMiddle = 0; dMiddle < nHigh; ++dMiddle) {
+                    long d = 10 * nHigh * dn + 10 * dMiddle + d0;
+                    if (d >= dLimit) {
+                        //System.err.printf("  limit reached for %d%n", d);
+                        break;
+                    }
+                    long n = d;
+                    for (int i = 0; i < nDigits; ++i) {
+                        digits[nDigits-1-i] = (int)(n % 10);
+                        n /= 10;
+                    }
+                    n = number(digits);
+                    if (d * a == n) {
+                        System.out.println("Solution: " + d + " * " + a + " = " + n);
+                    }
                 }
             }
         }
@@ -63,7 +72,7 @@ public class Micropuzzle34
         } else {
             for (int i = 0; i < args.length; ++i) {
                 int nDigits = Integer.parseInt(args[i]);
-                if (nDigits >= 1 && nDigits <= 9) {
+                if (nDigits >= 2 && nDigits <= 18) {
                     search(nDigits);
                 }
             }

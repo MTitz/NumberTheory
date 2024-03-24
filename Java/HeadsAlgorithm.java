2864b3c9cd6b19603723d1213c38a40c202ecc4f
Martin Titz <martin.titz@gmail.com>
2024-03-24 18:30:02 +0100

    Use floorMod instead of % operator to avoid conditions in case of negative value of t

--- Java/HeadsAlgorithm.java
+++ Java/HeadsAlgorithm.java
@@ -21,17 +21,15 @@ public class HeadsAlgorithm {
         long b = x - a * T;
         long c = y / T;
         long d = y - c * T;
-        long z = (a * d + b * c) % m;
+        long z = floorMod(a * d + b * c, m);
         long e = (a * c) / T;
         long f = a * c - e * T;
-        long v = (z + e * t) % m;
-        if (v < 0) v += m;
+        long v = floorMod(z + e * t, m);
         long g = v / T;
         long h = v - g * T;
-        long j = ((f + g) * t) % m;
-        if (j < 0) j += m;
-        long k = (j + b * d) % m;
-        return (h * T + k) % m;
+        long j = floorMod((f + g) * t, m);
+        long k = floorMod(j + b * d, m);
+        return floorMod(h * T + k, m);
     }
 
     private final long m;

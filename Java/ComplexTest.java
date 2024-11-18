576b8ad4062cbd0e21e348fbbf87094da0adb232
Martin Titz <martin.titz@gmail.com>
2024-11-18 10:34:44 +0100

    Add a few tests for division, sine function and output

--- Java/ComplexTest.java
+++ Java/ComplexTest.java
@@ -29,8 +29,9 @@ public class ComplexTest extends TestCase {
     }
 
     public void testDivide() {
-        assertEquals(new Complex(0.5, 0.5), new Complex(1, 1).divide(2.0));
+        assertEquals(new Complex(0.5,  0.5), new Complex(1, 1).divide(2.0));
         assertEquals(new Complex(0.7, -0.1), new Complex(3, 1).divide(new Complex(4, 2)), DELTA);
+        assertEquals(new Complex(2, -1), new Complex(15).divide(new Complex(6, 3)), DELTA);
     }
 
     public void testSqrt() {
@@ -44,6 +45,14 @@ public class ComplexTest extends TestCase {
         assertEquals(new Complex(+1.0, 0.0), Complex.pow(Complex.I, 4), DELTA);
         assertEquals(new Complex(0.20787957635076193), Complex.pow(Complex.I, Complex.I), DELTA);
         assertEquals(new Complex(exp(-PI/2.0)), Complex.pow(Complex.I, Complex.I), DELTA);
+        assertEquals(new Complex(-0.0022215597274345463, 0.008337128759360024), Complex.pow(new Complex(0.4, 0.3), new Complex(5.0, 2.0)), DELTA);
+    }
+
+    public void testSin() {
+        assertEquals(new Complex(0.0, 0.0), Complex.sin(Complex.ZERO), DELTA);
+        assertEquals(new Complex(0.09983341664682815, 0.0), Complex.sin(new Complex(0.1)), DELTA);
+        assertEquals(new Complex(0.0, 0.10016675001984403), Complex.sin(new Complex(0.0, 0.1)), DELTA);
+        assertEquals(new Complex(0.10033299984131393, 0.09966633349210759), Complex.sin(new Complex(0.1, 0.1)), DELTA);
     }
 
     public void testToString() {
@@ -51,5 +60,7 @@ public class ComplexTest extends TestCase {
         assertEquals("(1.0 + 0.0i)", Complex.ONE.toString());
         assertEquals("(1.0 + 1.0i)", Complex.ONE.add(Complex.I).toString());
         assertEquals("(1.0 - 1.0i)", Complex.ONE.subtract(Complex.I).toString());
+        assertEquals("(0.3 - 0.4i)", new Complex(0.3, -0.4).toString());
+        assertEquals("(3.0 + 4.0i)", new Complex(3.0, 4.0).toString());
     }
 }

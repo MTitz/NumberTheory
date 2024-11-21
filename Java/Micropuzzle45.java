93160a40408265bc062880f294fc9a22ff9a61c2
Martin Titz <martin.titz@gmail.com>
2024-11-21 22:07:26 +0100

    Use isEmpty() instead of size(). Rename a variable.

--- Java/Micropuzzle45.java
+++ Java/Micropuzzle45.java
@@ -26,7 +26,7 @@ public class Micropuzzle45
         int maxCount = -1;  // not yet found any maximum
         int maxNumber = 0;
         final int ITERATION_LIMIT = 1000;
-        SortedSet<Integer> exceptionalCase = new TreeSet<>();
+        SortedSet<Integer> exceptionalCases = new TreeSet<>();
         loop: for (int n = 10; n <= nMax; ++n) {
             BigInteger number = BigInteger.valueOf(n);
             BigInteger reverseNumber = reverseDigits(number);
@@ -34,7 +34,7 @@ public class Micropuzzle45
             while (number.compareTo(reverseNumber) != 0) {
                 ++count;
                 if (count > ITERATION_LIMIT) {
-                    exceptionalCase.add(n);
+                    exceptionalCases.add(n);
                     continue loop;
                 }
                 number = number.add(reverseNumber);
@@ -51,12 +51,12 @@ public class Micropuzzle45
             System.out.println("Maximum of " + maxCount +
                     " iterations reached for number " + maxNumber);
         }
-        if (exceptionalCase.size() > 0) {
+        if (!exceptionalCases.isEmpty()) {
             final int NUMBERS_PER_LINE = 8;
             System.out.println();
             System.out.println("No palindrome after " + ITERATION_LIMIT + " iterations:");
             int outputCount = 0;
-            for (Integer n : exceptionalCase) {
+            for (Integer n : exceptionalCases) {
                 System.out.printf(" %7d", n);
                 ++outputCount;
                 if (outputCount % NUMBERS_PER_LINE == 0)

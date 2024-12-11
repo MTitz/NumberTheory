42263a982dc2919d9953e291854f69f02075b586
Martin Titz <martin.titz@gmail.com>
2024-12-11 15:48:07 +0100

    Simplify program for solving micropuzzle 61 in book by J.J.Clessa

--- Java/Micropuzzle61.java
+++ Java/Micropuzzle61.java
@@ -1,5 +1,6 @@
 /* Program Micropuzzle61.java to solve micropuzzle 61 from
    J. J. Clessa "Math and Logic Puzzles for PC Enthusiasts"
+   The hymn board
 */
 
 public class Micropuzzle61
@@ -10,27 +11,21 @@ public class Micropuzzle61
         final int n = numbers.length;
         final int dmax = numbers[0];  // could also use minimum of numbers
         int[] remainder = new int[n];
+      divisors:
         for (int d = 1; d <= dmax; ++d) {
-            for (int i = 0; i < n; ++i) {
-                int quotient = numbers[i] / d;
-                remainder[i] = numbers[i] - d * quotient;
-            }
+            remainder[0] = numbers[0] % d;
 
-            // Check if we have a possible solution: all remainders must be equal
-            boolean isSolution = true;
-            for (int j = 1; j < n; ++j) {
-                if (remainder[j] != remainder[0]) {
-                    isSolution = false;
-                    break;
-                }
-            }
             // Remainder 0 not valid as door number
             if (remainder[0] == 0)
-                isSolution = false;
+                continue divisors;
 
-            if (isSolution) {
-                System.out.println("Solution: House number " + d + ", brother's door number: " + remainder[0]);
+            for (int i = 1; i < n; ++i) {
+                remainder[i] = numbers[i] % d;
+                if (remainder[i] != remainder[0])
+                    continue divisors;
             }
+
+            System.out.println("Solution: House number " + d + ", brother's door number: " + remainder[0]);
         }
     }
 }

8904adb117a8d36d4bdd0cd679b479d8a438d80d
Martin Titz <martin.titz@gmail.com>
2025-08-30 10:12:22 +0200

    Allow other values than 1296 in original micropuzzle 57

--- Java/Micropuzzle57.java
+++ Java/Micropuzzle57.java
@@ -1,5 +1,6 @@
 /* Program Micropuzzle57.java to solve micropuzzle 57 from
    J. J. Clessa "Math and Logic Puzzles for PC Enthusiasts"
+   The census taker
 */
 
 import java.util.Set;
@@ -7,10 +8,10 @@ import java.util.HashSet;
 
 public class Micropuzzle57
 {
-    public static void main(String[] args)
+    public static void solve(long product)
     {
-        final long PRODUCT = 1296;
-        long[] divisors = NumberTheory.divisors(PRODUCT);
+        System.out.printf("%nLooking for solutions for product %d%n", product);
+        long[] divisors = NumberTheory.divisors(product);
         Set<Long> found = new HashSet<>();
         for (int i = 0; i < divisors.length; ++i) {
             long age1 = divisors[i];
@@ -18,15 +19,30 @@ public class Micropuzzle57
                 long age2 = divisors[j];
                 for (int k = j; k < divisors.length; ++k) {
                     long age3 = divisors[k];
-                    if (age1 * age2 * age3 == PRODUCT) {
+                    if (age1 * age2 * age3 == product) {
                         long sum = age1 + age2 + age3;
-                        System.out.printf("%4d %4d %4d    %4d%n", age1, age2, age3, sum);
+                        System.out.printf("%6d %6d %6d    %6d%n", age1, age2, age3, sum);
                         if (found.contains(sum)) {
-                            System.out.println("Solution found with door number " + sum);
+                            System.out.println(" (solution found with door number " + sum + ")");
                         } else {
                             found.add(sum);
                         }
                     }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        final long PRODUCT = 1296;  // the value given in micropuzzle 57
+        if (args.length == 0) {
+            solve(PRODUCT);
+        } else {
+            for (int i = 0; i < args.length; ++i) {
+                long n = Long.parseLong(args[i]);
+                if (n > 0) {
+                    solve(n);
                 }
             }
         }
